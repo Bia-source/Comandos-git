402067c36ffbc9539637bfb66b4d5f21ea78e58c
Bia-source <bia_ferreirads@yahoo.com>
2024-03-14 20:30:47 -0300

    adicionando function timeoutCreateCard

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ function somar(){
     return 2+3;
 }
 
+function timeoutCreateCard(){
+    // 1. Retornar 3 coisas
+    // 1.1 Um objeto
+    // 1.2 Uma stringjson
+    // 1.3 Apenas o valor da mensagem tudo em caixa alta 
+
+    // 1.1 = {message: 'Hello World!'}
+    // 1.2 = '{message: 'Hello World!'}'
+    // 1.3 = 'HELLO WORLD!')
+    fetch("https://loginvalidatorapi-mmwp.onrender.com/")
+    .then(res => res.json())
+    .then(data => {
+        // console.log({message: 'Hello World!'})
+        // console.log('{message: 'Hello World!'}')
+        // console.log('HELLO WORLD!')
+    })
+}
+
+
 function carrinho(){
     console.log("adicionando produtos no carrinho")
 }
